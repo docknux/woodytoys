c0a4f303f832899dd525e602bb7bf768a8a7c430
Denis Pettens <denis.pettens@gmail.com>
2017-05-31 16:56:31 +0200

    [vps-config] Finalize letsencrypt script

--- vps-config/letsencrypt.sh
+++ vps-config/letsencrypt.sh
@@ -5,7 +5,7 @@ iptables -I INPUT  1 -p tcp --dport 443 -j ACCEPT
 iptables -I OUTPUT 1 -p tcp --sport 443 -j ACCEPT
 
 # create certificates for wt6.ephec-ti.be and subdomains www, b2b, mail, voip
-certbot certonly -n --agree-tos --test-cert --standalone --email sysadmin@wt6.ephec-ti.be \
+certbot certonly -n --agree-tos --standalone --email sysadmin@wt6.ephec-ti.be \
     -d wt6.ephec-ti.be -d www.wt6.ephec-ti.be \
     -d b2b.wt6.ephec-ti.be \
     -d mail.wt6.ephec-ti.be \
