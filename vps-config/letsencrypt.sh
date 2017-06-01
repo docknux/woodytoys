a91fa415a242669f89bccfd034938b6d25ae1e05
Denis Pettens <denis.pettens@gmail.com>
2017-06-01 15:57:01 +0200

    [vps-config] Create separate ssl certificates for mail and voip

--- vps-config/letsencrypt.sh
+++ vps-config/letsencrypt.sh
@@ -7,13 +7,19 @@
 iptables -I INPUT  1 -p tcp --dport 443 -j ACCEPT
 iptables -I OUTPUT 1 -p tcp --sport 443 -j ACCEPT
 
-# create certificates for wt6.ephec-ti.be and subdomains www, b2b, mail, voip
+# create certificates for wt6.ephec-ti.be and subdomains www, b2b
 certbot certonly -n --agree-tos --standalone --email sysadmin@wt6.ephec-ti.be \
     -d wt6.ephec-ti.be \
     -d www.wt6.ephec-ti.be \
     -d b2b.wt6.ephec-ti.be \
+
+# create certificates for mailwt6.ephec-ti.be
+certbot certonly -n --agree-tos --standalone --email sysadmin@wt6.ephec-ti.be \
     -d mail.wt6.ephec-ti.be \
-    -d voip.wt6.ephec-ti.be
+
+# create certificates for voip.wt6.ephec-ti.be
+certbot certonly -n --agree-tos --standalone --email sysadmin@wt6.ephec-ti.be \
+    -d voip.wt6.ephec-ti.be \
 
 # Remove iptables rules created for certbot
 iptables -D INPUT 1
