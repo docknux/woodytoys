666a0edc6564266e3a02b833947e3f47b9a022ac
Denis Pettens <denis.pettens@gmail.com>
2017-06-01 09:54:16 +0200

    [vps-config] Rework letsencrypt script

--- vps-config/letsencrypt.sh
+++ vps-config/letsencrypt.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+# Stop apache-proxy
+#docker-compose stop apache-proxy
+
 # Allow certbot to connect to port 443
 iptables -I INPUT  1 -p tcp --dport 443 -j ACCEPT
 iptables -I OUTPUT 1 -p tcp --sport 443 -j ACCEPT
 
 # create certificates for wt6.ephec-ti.be and subdomains www, b2b, mail, voip
 certbot certonly -n --agree-tos --standalone --email sysadmin@wt6.ephec-ti.be \
-    -d wt6.ephec-ti.be -d www.wt6.ephec-ti.be \
+    -d wt6.ephec-ti.be \
+    -d www.wt6.ephec-ti.be \
     -d b2b.wt6.ephec-ti.be \
     -d mail.wt6.ephec-ti.be \
     -d voip.wt6.ephec-ti.be
@@ -14,3 +18,6 @@ certbot certonly -n --agree-tos --standalone --email sysadmin@wt6.ephec-ti.be \
 # Remove iptables rules created for certbot
 iptables -D INPUT 1
 iptables -D OUTPUT 1
+
+# Restart apache-proxy
+#docker-compose start apache-proxy
